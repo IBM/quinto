8d76adfde6c83ef70592dc61da3da89664b4b5a1
metonym <ericyl.us@gmail.com>
2019-05-08 09:28:34 -0700

    refactor(example): use quinto from npm
    Remove `emotion` and use plain css. Prefer cache used by parcel.

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,14 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { injectGlobal } from 'emotion';
+import './styles.css';
 import App from './App';
-
-injectGlobal`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
 
 render(<App />, document.getElementById('root'));
