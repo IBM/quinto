25f194947beb897c47522d51e826a6fe49223fad
Eric Liu <ericyl.us@gmail.com>
2019-07-17 12:45:26 -0700

    chore: move jest config to package.json

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>src/setupTests.ts'],
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>src/**/*.(spec|test).{ts,tsx}']
-};
